72cdf8c4bfd1a2d27a1b251c0c32a5fdee513a78
Wenyuan Zhu <1160128326@qq.com>
2025-12-18 13:15:04 +0000

    corresponding model names in loo visualization

--- analysis_loo.R
+++ analysis_loo.R
@@ -1,51 +1,81 @@
-# chosen_exp = 'Alex'
-chosen_exp = 'Divyaj'
+library(loo)
+library(ggplot2)
+
+chosen_exp = 'Alex'
+# chosen_exp = 'Divyaj'
 # chosen_exp = 'Exp6'
 
-fit_log_basic = read_rds(paste('./results/',chosen_exp,'/log_basic.rds', sep = ''), refhook = NULL)
-fit_log_seq = read_rds(paste('./results/',chosen_exp,'/log_seq.rds', sep = ''), refhook = NULL)
+# fit_log_basic = read_rds(paste('./results/',chosen_exp,'/log_basic.rds', sep = ''), refhook = NULL)
+# fit_log_seq = read_rds(paste('./results/',chosen_exp,'/log_seq.rds', sep = ''), refhook = NULL)
 fit_basic = read_rds(paste('./results/',chosen_exp,'/basic.rds', sep = ''), refhook = NULL)
-fit_basic_seq = read_rds(paste('./results/',chosen_exp,'/basic_seq.rds', sep = ''), refhook = NULL)
+# fit_basic_seq = read_rds(paste('./results/',chosen_exp,'/basic_seq.rds', sep = ''), refhook = NULL)
 
 # log_lik <- extract(fit_basic)$log_lik
 # loo_basic_result <- loo(log_lik, r_eff = NA)
 loo_basic_result <- loo(fit_basic)
 
-loo_basic_seq_result <- loo(fit_basic_seq)
+load(paste0('results/',chosen_exp,'/loo/loo_basic_seq.rdata')) 
+loo_basic_seq_result <- loo
 
-loo_log_basic_result <- loo(fit_log_basic)
+# 
+# loo = loo(fit_log_basic)
+# save(loo, file = paste('./results/',chosen_exp,'/loo/loo_log_basic.rdata',sep = ''))
 
-loo_log_seq_result <- loo(fit_log_seq)
+load(paste0('results/',chosen_exp,'/loo/loo_log_basic.rdata')) 
+loo_log_basic_result <- loo
+
+load(paste0('results/',chosen_exp,'/loo/loo_log_seq.rdata')) 
+loo_log_seq_result <- loo
 
 
-loo_basic_result$estimates[1,1]
-loo_basic_seq_result$estimates[1,1]
-loo_log_basic_result$estimates[1,1]
-loo_log_seq_result$estimates[1,1]
+loo_basic_result$estimates
+loo_basic_seq_result$estimates
+loo_log_basic_result$estimates
+loo_log_seq_result$estimates
 
-df <- data.frame(
-  model.name = c('BDT', 'Seq.weights','Rel.distortion','Full model'),
-  elpd_ = c(loo_basic_result$estimates[1,1],
-              loo_basic_seq_result$estimates[1,1],
-              loo_log_basic_result$estimates[1,1],
-              loo_log_seq_result$estimates[1,1]),
-  SE = c(loo_basic_result$estimates[1,2],
-         loo_basic_seq_result$estimates[1,2],
-         loo_log_basic_result$estimates[1,2],
-         loo_log_seq_result$estimates[1,2])
+comp = loo_compare(loo_basic_result, 
+                   loo_basic_seq_result, 
+                   loo_log_basic_result,  
+                   loo_log_seq_result)
+
+
+
+
+df = data.frame(comp[,c(1,2)])
+
+name_map <- c(
+  model1 = "BDT",
+  model2 = "Seq.weights",
+  model3 = "Rel.distortion",
+  model4 = "Full model"
 )
 
-df$model.name = factor(df$model.name, levels = c(df$model.name))
-df$ci_low  <- df$elpd_ - 1.96 * df$SE
-df$ci_high <- df$elpd_ + 1.96 * df$SE
+model.name = name_map[row.names(comp)]
 
-library(ggplot2)
+df = cbind(df, model.name)
+df$model.name <- factor(
+  df$model.name,
+  levels = rev(unique(df$model.name))
+)
+df$ci_low  <- df$elpd_diff - 1.96 * df$se_diff
+df$ci_high <- df$elpd_diff + 1.96 * df$se_diff
 
 
-ggplot(df, aes(x = model.name, y = elpd_)) +
-  geom_col(fill = 'gray') +
+ggplot(df, aes(x = model.name, y = elpd_diff)) +
+  geom_col(fill = 'gray50') +
   geom_errorbar(aes(ymin = ci_low, ymax = ci_high), width = 0.1) +
   theme_minimal() +
-  labs(
-    title = chosen_exp)
+  ylab('expected log predictive density difference') +
+  labs(title = chosen_exp)
+
+
+#### sanity check
+# 
+# elpd_diff_manual <-
+#   loo_log_seq_result$pointwise[, "elpd_loo"] -
+#   loo_log_basic_result$pointwise[, "elpd_loo"]
+# 
+# sum(elpd_diff_manual)
+
+
 
